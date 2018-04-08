49102564eb93c083a213354f7dd22c23d98b4844
lnj34 <ljibril18@gmail.com>
2018-04-07 19:30:36 -0500

    Main.c file created

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=lnj34_ME433_2018.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/lnj34_ME433_2018.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=lnj34me4332018.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/lnj34me4332018.tar
